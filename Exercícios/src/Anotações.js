af58dc3b4773b4ba688b594e86888a2fb9cd50f0
Natalia Mota <nataliamota@cpejr.com.br>
2022-10-25 22:41:42 -0300

    Anotações iniciais

---
+++ Exercícios/src/Anotações.js
@@ -0,0 +1,47 @@
+  //VARIAVEIS
+  //let - variaveis 
+  //const - usar maioria das vzs
+  //var - variavel fora de escopo
+
+  //FUNCAO
+  //const funcao = () => {
+  //  alert ("Você clicou aqui!")
+  //};
+
+  //VETOR
+  //const nomeExercicioSuperior = ["Cerrote ", "Extensão Tríceps ", "Flexão "];
+  //const nomeExercicioInferior = ["Agachamento Livre ", "Leg Press ", "Cadeira Extensora "]
+  //const nomeExercicioAlongamento = ["Quadríceps e Coluna ", "Posterior de Ombro ", "Downward Dog "]
+
+  /*const endereco = {
+    rua : "Dolores",
+    numero : 504,
+  };
+  const pessoa = {
+    nome : "Arthur",
+    idade : 19,
+    altura : 1.65,
+    endereco : endereco,
+    notasFaculdade : [10, 2, 10, 2],
+
+  }; 
+
+  function clicou () {
+    alert ("Execício adicionado a sua lista!")
+  }
+  
+  return (
+    <div>
+      <h1>Exercícios</h1>
+
+      <h2>Musculação</h2>
+
+      <h3>Superiores</h3>
+
+      <h3>Inferiores</h3>
+
+      <h2>Alongamentos</h2>
+
+      <button onClick={clicou}> + Adicionar </button>
+    </div>
+  ); */
