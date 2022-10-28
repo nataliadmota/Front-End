86bbb2749f7e7c028acf08461d9c288e4cb1fe86
gabrielglm1 <gabriellmend51@gmail.com>
2022-10-28 09:50:27 -0300

    att

---
+++ smartgym/src/routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Cadastro from './pages/Cadastro';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Exercicios from './pages/Exercicios';
+import Perfil from './pages/Perfil'; 
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' element={Login}/>
+                <Route path='/' element={Home}/>
+                <Route path='/' element={Cadastro}/>
+                <Route path='/' element={Exercicios}/>
+                <Route path='/' element={Perfil}/>
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes; 
