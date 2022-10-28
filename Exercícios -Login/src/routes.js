a306b90f01ce10ae48c70d9c4ed51ba188d05d6e
Natalia Mota <nataliamota@cpejr.com.br>
2022-10-28 09:59:49 -0300

    Rotas atualizado
    inclusão cadastro, home e perfil nas rotas

--- Exercícios -Login/src/routes.js
+++ Exercícios -Login/src/routes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Exercicios from "./Pages/Exercicios";
-import Login from "./Pages/Login";
+import Cadastro from "./Pages/Cadastro";
+import Home from "./Pages/Home";
+import Perfil from "./Pages/Perfil";
 
 function Routes () {
     return (
@@ -9,6 +11,9 @@ function Routes () {
             <Switch>
                 <Route path = "/exercicios" component = {Exercicios}/>
                 <Route path = "/login" component = {Login}/>
+                <Route path = "/cadastro" component = {Cadastro}/>
+                <Route path = "/home" component = {Home}/>
+                <Route path = "/perfil" component = {Perfil}/>
             </Switch>
         </BrowserRouter>
     )
